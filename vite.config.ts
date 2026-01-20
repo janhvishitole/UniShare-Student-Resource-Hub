f2348187f04cafa43f18f4c2920765322d05939b
janhvishitole <janhvishitole@gmail.com>
2026-01-20 19:33:43 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   return {
-    base: '/UniShare-Student-Resource-Hub/', // This fixes the blank screen
+    base: '/UniShare-Student-Resource-Hub/', 
     server: {
       port: 3000,
       host: '0.0.0.0',
